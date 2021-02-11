958e467368a0bca20fa4c6fc74a8d595fb099d6e
Akhir Alibhai <akhir.alibhai@gmail.com>
2021-02-11 11:49:11 -0500

    Add remaining interface types

--- src/interfaces.jl
+++ src/interfaces.jl
@@ -1,5 +1,5 @@
 export pcap_lookupdev,
-        sockaddr
+        sockaddr, pcap_addr, pcap_if
 
 function pcap_lookupdev()
     err = Ptr{Int8}()
@@ -15,4 +15,25 @@ struct sockaddr
     sa_family::Cushort
     se_data::Cstring
     sockaddr() = new(0, Base.unsafe_convert(Cstring, ""))
+end
+
+struct pcap_addr
+    next::Ptr{pcap_addr}
+    addr::Ref{sockaddr}
+    netmask::Ref{sockaddr}
+    broadaddr::Ref{sockaddr}
+    dstaddr::Ref{sockaddr}
+    pcap_addr() = new(Ptr{pcap_addr}(), Ref(sockaddr()), Ref(sockaddr()),
+                                        Ref(sockaddr()), Ref(sockaddr()))
+end
+
+struct pcap_if
+    next::Ptr{pcap_if}
+    name::Cstring
+    description::Cstring
+    addresses::Ref{pcap_addr}
+    flags::UInt
+    pcap_if() = new(Ptr{pcap_if}(), Base.unsafe_convert(Cstring, ""),
+                    Base.unsafe_convert(Cstring, ""), Ref(pcap_addr()),
+                    0)
 end
