5af9c631f4ffad8af8e144cdd57701744218d04b
Akhir Alibhai <akhir.alibhai@gmail.com>
2021-02-23 21:48:12 -0500

    Rename attributes to match naming conventions

--- src/types/internetHdrs.jl
+++ src/types/internetHdrs.jl
@@ -5,14 +5,14 @@ struct Ipv4Hdr
     ihl::UInt8
     dscp::UInt8
     ecn::UInt8
-    totalLength::UInt16
+    total_length::UInt16
     identification::UInt16
     flags::UInt8
-    fragmentOffset::UInt16
-    timeToLive::UInt8
+    fragment_offset::UInt16
+    time_to_live::UInt8
     protocol::UInt8
-    headerChecksum::UInt16
-    sourceIp::AbstractString # IPv4
-    destinationIp::AbstractString # IPv4
+    header_checksum::UInt16
+    source_ip::AbstractString # IPv4
+    destination_ip::AbstractString # IPv4
     options::AbstractString # Perform more research on this value
 end
