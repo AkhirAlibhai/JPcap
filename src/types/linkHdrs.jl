7250d9099ba54b1a827645f7d6d399c80eeb8e44
Akhir Alibhai <akhir.alibhai@gmail.com>
2021-03-02 13:03:38 -0500

    Add APR header struct

---
+++ src/types/linkHdrs.jl
@@ -0,0 +1,14 @@
+export ArpHdr
+
+struct ArpHdr
+    hardware_type::UInt16
+    protocol_type::UInt16
+    hardware_address_length::UInt8
+    protocol_address_length::UInt8
+    operation::UInt16
+    sender_hardware_address::UInt32 # 24 bits
+    sender_protocol_address::UInt16
+    target_hardware_address::UInt32 # 24 bits
+    target_protocol_address::UInt16
+    # TODO: Potentially do the same bitmask thing in transportHdrs.jl
+end
