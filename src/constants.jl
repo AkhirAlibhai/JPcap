df03b1373265e4d7123036323d69fa11899888ef
Akhir Alibhai <akhir.alibhai@gmail.com>
2021-03-07 18:45:47 -0500

    Add pcap_activate

--- src/constants.jl
+++ src/constants.jl
@@ -3,10 +3,10 @@ export PCAP_ERROR, PCAP_WARNING
 """
     Error codes for the pcap API that are returned from multiple functions
 """
-const PCAP_ERROR =      -1 # generic error code
+const PCAP_ERROR =      -1  # generic error code
 
 
 """
     Warning codes for the pcap API that are returned from multiple functions
 """
-const PCAP_WARNING =    1 # generic warning code
+const PCAP_WARNING =    1   # generic warning code
--- src/sending.jl
+++ src/sending.jl
@@ -1,9 +1,9 @@
-export pcap_create
+export pcap_create, pcap_activate
 
 mutable struct pcap_t
 end
 
-function pcap_create(source::String)::pcap_t
+function pcap_create(source::String)::Ptr{pcap_t}
     # Creates a live capture handle for the given interface
     err = Ptr{Int8}()
 
@@ -14,5 +14,18 @@ function pcap_create(source::String)::pcap_t
         print("Error occured when attempting to create live capture handle: ", unsafe_string(err))
         return nothing
     end
-    loaded_handle
+    handle
+end
+
+const PCAP_ERROR_ACTIVATED =        -4  # the operation can't be performed on already activated captures
+const PCAP_ERROR_NO_SUCH_DEVICE =   -5	# no such device exists
+const PCAP_ERROR_RFMON_NOTSUP =     -6	# this device doesn't support rfmon (monitor) mode
+const PCAP_ERROR_PERM_DENIED =      -8	# no permission to open the device
+const PCAP_ERROR_IFACE_NOT_UP =     -9  # interface isn't up
+
+const PCAP_WARNING_PROMISC_NOTSUP = 2	# this device doesn't support promiscuous mode
+
+function pcap_activate(p::Ptr{pcap_t})::Int32
+    # Activates a capture handle
+    ccall((:pcap_activate, "libpcap"), Int8, (Ptr{pcap_t},), p)
 end
