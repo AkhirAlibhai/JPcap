c69f8c2437f36647eb641ad291ba239b92a3b517
Akhir Alibhai <akhir.alibhai@gmail.com>
2021-03-06 22:55:51 -0500

    Rename val to handle

--- src/sending.jl
+++ src/sending.jl
@@ -7,12 +7,12 @@ function pcap_create(source::String)::pcap_t
     # Creates a live capture handle for the given interface
     err = Ptr{Int8}()
 
-    val = ccall((:pcap_create, "libpcap"), Ptr{pcap_t}, (Cstring, Ptr{Int8}), Base.cconvert(Cstring, source), err)
+    handle = ccall((:pcap_create, "libpcap"), Ptr{pcap_t}, (Cstring, Ptr{Int8}), Base.cconvert(Cstring, source), err)
 
-    loaded_pcap_t = unsafe_load(val)
-    if loaded_pcap_t == C_NULL
+    loaded_handle = unsafe_load(handle)
+    if loaded_handle == C_NULL
         print("Error occured when attempting to create live capture handle: ", unsafe_string(err))
         return nothing
     end
-    loaded_pcap_t
+    loaded_handle
 end
